0b07cc21654f45665330dec721bbda72b9238518
webdevyoo <yjk7454@naver.com>
2025-06-16 00:10:58 +0900

    feat: `main.js` - router 모듈 엔트리포인트 등록

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // import Vue from "vue";
 import { createApp } from "vue";
 import App from "./App";
-
-createApp(App).mount("#app");
+import router from './routes/index.js'
+createApp(App)
+.use(router)
+.mount("#app");
