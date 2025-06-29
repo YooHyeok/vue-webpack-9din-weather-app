a004a157ac26e5019633bc4d395d1ca6791e9c9c
webdevyoo <yjk7454@naver.com>
2025-06-29 22:34:57 +0900

    feat: `main.js` - vuex store 모듈 엔트리포인트 등록

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 import { createApp } from "vue";
 import App from "./App";
 import router from './routes/index.js'
+import store from "./store/index.js";
 createApp(App)
 .use(router)
+.use(store)
 .mount("#app");
