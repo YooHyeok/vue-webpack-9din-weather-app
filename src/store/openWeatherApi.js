aef33cb47ff77ed83ff014442c79294d5eb4c92f
webdevyoo <yjk7454@naver.com>
2025-06-30 00:01:26 +0900

    feat: `store/openWeatherApi.js` 날씨 api 호출 FETCH_OPENWEATHER_API actions 정의

--- src/store/openWeatherApi.js
+++ src/store/openWeatherApi.js
@@ -5,7 +5,22 @@ export default {
   /* [namespaced]: 하나의 store에서 모듈화 하여 사용할 수 있음을 명시적으로 나타내는 개념 */
   namespaced: true,
   /* [state]: 실제로 취급해야하는 데이터 */
-  state: {},
+  state: {
+    cityName: 'Seoul',
+    currentWeather: {
+      /* MainView.vue */
+      currentTemp: 0,
+      currentHumidity: 0,
+      currentWindSpeed: 0,
+      currentFeelsLike: 0,
+      /* SubView.vue */
+      currentSunrise: 0,
+      currentSunset: 0,
+      currentVisibility: 0,
+    },
+    hourlyWeather: [],
+    images: [],
+  },
   /* [getters]: 계산된 상태의 데이터를 만들어내는 속성 - computed와 유사한 기능 */
   getters: {},
   /* [mutations]: 변이 메소드, 우리가 관리하는 데이터인 state를 오로지 이곳 mutations 에서만 변경시킬 수 있다. */
@@ -18,6 +33,33 @@ export default {
       - 객체 구조 분해 할당을 통해 불러올 수도 있다.
   */
   actions: {
+    // OPENWEATHERAPI 데이터호출
+    async FETCH_OPENWEATHER_API(context) {
+      try {
+        const API_KEY = "32c7ade76f0e3f495584cbb0d0cd1efe";
+        let initialLat = 36.5683;
+        let initialLon = 126.9778;
+        const ENDPOINT = {
+          WEATHER: 'weather',
+          FORECAST: 'forecast',
+        }
+        const api_url = (endpoint) => `https://api.openweathermap.org/data/2.5/${endpoint}?lat=${initialLat}&lon=${initialLon}&appid=${API_KEY}&units=metric`
+        // get() 메서드를 통해서 우리가 필요로하는 API 데이터를 호출한다.
+        const res1 = await axios.get(api_url(ENDPOINT.WEATHER))
+
+        // 시간대별 날씨 데이터 제어
+        const res2 = await axios.get(api_url(ENDPOINT.FORECAST))
+
+        const images = new Array()
+        for (let i = 0; i < 48; i++) {
+          const weatherIcon = res2.data.list[0].weather[0].icon
+          images[i] = `src/assets/images${weatherIcon}.png`
+        }
+      } catch(error) {
+        console.error(error)
+      }
+      
+    }
   }
 
 }
