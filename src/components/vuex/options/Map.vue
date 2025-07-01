d3223e456ab98edef86dabadc9c3362e9db81895
webdevyoo <yjk7454@naver.com>
2025-07-02 00:04:35 +0900

    feat: `Map.vue` - 카카오 맵 api 마커 position json import 적용 및 마커 클릭 지역 날씨 조회

---
+++ src/components/vuex/options/Map.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="mapContainer">
+    <!-- map이 그려질 영역 -->
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+import MapPositions from "~/assets/map-positions.json"
+export default {
+  mounted() {
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    } else {
+      const script = document.createElement('script');
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      /* https://greed-yb.tistory.com/335 */
+      script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=110d1acf7c25542f27e793583a5c26ec';
+      document.head.appendChild(script);
+    }
+  },
+  methods: {
+    initMap() {
+      let mapContainer = document.getElementById('map'), // 지도를 표시할 div
+      mapOption = {
+        // center: new kakao.maps.LatLng(37.564343, 126.947613), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(37.73035, 127.967487), // 지도의 중심좌표
+        level: 13, // 지도의 확대 레벨
+      };
+
+      let map = new kakao.maps.Map(mapContainer, mapOption);
+      const positions = MapPositions.map((pos) => ({
+        latlng: new kakao.maps.LatLng(...pos.latlng),
+        cityName: pos.cityName,
+      }))
+      
+      // 마커를 생성합니다
+      positions.forEach(function (pos) {
+        console.log("pos: ", pos)
+        console.log("this: ", this)
+        let marker = new kakao.maps.Marker({
+          position: pos.latlng, // 마커의 위치
+        });
+        marker.setMap(map); // 마커가 지도에 표시되도록 set
+
+        // 마커를 클릭했을 때, 클릭된 마커의 좌표를 Vuex Store에 전달
+        kakao.maps.event.addListener(marker, "click", () => {
+          // 클릭한 위도, 경도 정보를 불러온 후 watch로 따로 빼지 않고, 직접 Vuex Store에 할당
+          this.$store.commit('openWeatherApi/SET_CITYNAME', pos.cityName);
+          this.$store.commit('openWeatherApi/SET_LATLON', marker.getPosition());
+          this.$store.dispatch('openWeatherApi/FETCH_OPENWEATHER_API');
+        })
+      }, this);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+#mapContainer {
+  @include center;
+  width: 100%;
+  height: 35%;
+
+  #map {
+    width: 80%;
+    height: 90%;
+    border-radius: 10px;
+  }
+}
+</style>
